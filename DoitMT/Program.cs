7c34f87bffb644dec99f7ed1bf0dd69aa5e57016
a.arabuli <a.arabuli@AARABULI-LP>
2024-01-11 19:58:56 +0400

    Init

---
+++ DoitMT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection;
+using System.Text;
+using System.Threading.Channels;
+
+namespace DoitMT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dispose
+            using (FileStream fs = new FileStream("example.txt", FileMode.Create))
+            {
+                Console.WriteLine("test");
+            }
+            Console.WriteLine();
+        }
+    }
+}
