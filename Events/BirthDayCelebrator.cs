e4b2e3d1195c93375c473597997935d8a3d456dc
a.arabuli <a.arabuli@AARABULI-LP>
2024-01-25 19:43:05 +0400

    Final part of Events

---
+++ Events/BirthDayCelebrator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class CakeCutEventArgs : EventArgs
+    {
+        public string Message { get;}
+
+        public CakeCutEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public class BirthDayCelebrator
+    {
+        public event EventHandler<CakeCutEventArgs> CakeCutEvent;
+
+        protected virtual void OnCakeCut(string message)
+        {
+            CakeCutEventArgs args = new CakeCutEventArgs(message);
+            CakeCutEvent?.Invoke(this, args);
+        }
+
+        public void CelebrateBirthDay()
+        {
+            OnCakeCut("Birthday cake cutting");
+        }
+    }
+
+    public class PartySubscriber
+    {
+        public bool IsSucbscibed { get; set; }
+        private void CakeCutEventHandler(object sender, CakeCutEventArgs e)
+        {
+            Console.WriteLine($"Cool! there is and {e.Message} happening!");
+        }
+
+        private void CakeCutEventHandler2(object sender, CakeCutEventArgs e)
+        {
+            Console.WriteLine($"Cool! there is and {e.Message} happening! + log");
+        }
+
+        public void Subscribe(BirthDayCelebrator birthDayCelebrator)
+        {
+            birthDayCelebrator.CakeCutEvent += CakeCutEventHandler;
+            birthDayCelebrator.CakeCutEvent += CakeCutEventHandler2;
+            IsSucbscibed = true;
+        }
+
+        public void UnSubscribe(BirthDayCelebrator birthDayCelebrator)
+        {
+            birthDayCelebrator.CakeCutEvent -= CakeCutEventHandler;
+            birthDayCelebrator.CakeCutEvent -= CakeCutEventHandler2;
+            IsSucbscibed = false;
+        }
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -4,7 +4,91 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region UsualEvent
+
+            //MyPublisher publisher = new MyPublisher();
+            //Subscriber subscriber = new Subscriber();
+
+            //subscriber.Subscribe(publisher);
+
+            //publisher.DoSomething();
+
+            //subscriber.UnSubscribe(publisher);
+
+            //publisher.DoSomething();
+
+            #endregion
+
+            #region BirthDayCelebrator
+
+            BirthDayCelebrator birthDayCelebrator = new BirthDayCelebrator();
+            PartySubscriber subscriber = new PartySubscriber();
+
+            subscriber.Subscribe(birthDayCelebrator);
+
+            birthDayCelebrator.CelebrateBirthDay();
+
+            if(subscriber.IsSucbscibed)
+            {
+                subscriber.Subscribe(birthDayCelebrator);
+            }
+            else
+            {
+                Console.WriteLine("You're already subscribed");
+            }
+
+            birthDayCelebrator.CelebrateBirthDay();
+
+
+
+            #endregion
+
         }
     }
+
+    public class MyEventArgs : EventArgs 
+    {
+        public string Message { get; }
+
+        public MyEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public class MyPublisher
+    {
+        public event EventHandler<MyEventArgs> Myevent;
+
+        protected virtual void OnMyevent(string message)
+        {
+            MyEventArgs args = new MyEventArgs(message);
+
+            Myevent?.Invoke(this, args);
+        }
+
+        public void DoSomething()
+        {
+            OnMyevent("Something happened!");
+        }
+    }
+
+    public class Subscriber
+    {
+        private void MyEventHandler(object sender, MyEventArgs e)
+        {
+            Console.WriteLine($"Event handled by subscriber: {e.Message}");
+        }
+
+        public void Subscribe(MyPublisher publisher)
+        {
+            publisher.Myevent += MyEventHandler;
+        }
+
+        public void UnSubscribe(MyPublisher publisher)
+        {
+            publisher.Myevent -= MyEventHandler;
+        }
+    }
+
 }
