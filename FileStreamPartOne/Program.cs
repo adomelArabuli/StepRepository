db2884611eb90bd2d1ccde85c97661a71c47742f
a.arabuli <a.arabuli@AARABULI-LP>
2024-02-08 20:04:24 +0400

    FileStreamPartOne

---
+++ FileStreamPartOne/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace FileStreamPartOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region FileStream
+            //string filePath = "C:\\Users\\a.arabuli\\Desktop\\FileStream.txt";
+
+            //using(FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            //{
+            //    byte[] data = System.Text.Encoding.UTF8.GetBytes("Hello");
+            //    fs.Write(data, 0, data.Length);
+            //}
+
+            //using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            //{
+            //    byte[] buffer = new byte[1024];
+            //    int bytesToRead = fs.Read(buffer, 0, buffer.Length);
+            //    string content = System.Text.Encoding.UTF8.GetString(buffer,0,bytesToRead);
+            //    Console.WriteLine($"File content: {content}");
+            //}
+
+            //using(StreamReader sr = new StreamReader(filePath))
+            //{
+            //    string line = sr.ReadLine();
+            //    Console.WriteLine(line);
+            //}
+
+            //using (StreamWriter sr = new StreamWriter(filePath))
+            //{
+            //    sr.WriteLine("Test");
+            //}
+            #endregion
+
+            #region MemoryStream
+            //byte[] initialData = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7 };
+
+            //using (MemoryStream ms = new MemoryStream(initialData))
+            //{
+            //    ms.Seek(0, SeekOrigin.Begin);
+            //    byte[] buffer = new byte[ms.Length];
+
+            //    ms.Read(buffer, 0, (int)ms.Length);
+            //    Console.WriteLine($"Initial data {BitConverter.ToString(buffer)}");
+
+            //    ms.Seek(0, SeekOrigin.Begin);
+            //    byte[] newData = { 0x6, 0x7, 0x8 };
+            //    ms.Write(newData, 0, newData.Length);
+
+            //    ms.Seek(0, SeekOrigin.Begin);
+            //    buffer = new byte[ms.Length];
+            //    ms.Read(buffer, 0, (int)ms.Length);
+            //    Console.WriteLine($"Initial data {BitConverter.ToString(buffer)}");
+            //}
+
+                  /* 
+                  1A3F = (1 * 16^3) + (10 * 16^2) + (3 * 16^1) + (15 * 16^0)
+                  = (1 * 4096) + (10 * 256) + (3 * 16) + (15 * 1)
+                  = 4096 + 2560 + 48 + 15
+                  = 6719
+                  */
+                  // 0 - 9 A-F
+            #endregion
+
+            #region BufferedStream
+
+            string filePath = "C:\\Users\\a.arabuli\\Desktop\\FileStream.txt";
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            using (BufferedStream bfs = new BufferedStream(fs))
+            {
+                byte[] data = System.Text.Encoding.UTF8.GetBytes("Strnig hello world");
+                bfs.Write(data, 0, data.Length);
+
+                bfs.Flush();
+
+                bfs.Seek(0, SeekOrigin.Begin);
+                byte[] buffer = new byte[1024];
+                int bytesRead = bfs.Read(buffer, 0, buffer.Length);
+
+                string content = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine(content);
+            }
+
+
+            #endregion
+        }
+    }
+}
---
+++ LINQ2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+
+namespace LINQ2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = { "gia", "nina", "tekla", "giorgi", "demetre" };
+
+            //var result = names.Where(s => s.Contains("i"));
+
+            //var filteredStudetns = from student in studentList
+            //                       where student.Age > 12 && student.Age < 20
+            //                       select student.StudentName;
+
+            //var filteredStudetns = studentList.Where(x => x.Age > 12 && x.Age < 20);
+            //var filteredStudetns = studentList.Where((s,i) =>
+            //{
+            //    if(i % 2 == 0)return true;
+            //    return false;
+            //});
+
+            //var filteredStudetns = from s in studentList
+            //                       where s.Age > 12
+            //                       where s.Age < 20
+            //                       select s;
+
+            var mixedList = new ArrayList()
+            {
+                'c',
+                "string",
+                "string1",
+                65,
+                654,
+                true,
+                false
+            };
+
+            var stringList = from s in mixedList.OfType<string>()
+                             select s;
+            var boolList = from s in mixedList.OfType<bool>()
+                             select s;
+
+            //var orderedByNameAsc = from s in studentList
+            //                    orderby s.StudentName
+            //                    select s;
+
+            //var orderedByNameDesc = from s in studentList
+            //                    orderby s.StudentName
+            //                    orderby s.Age
+            //                    select s;                 ?
+
+            //var orderedByNameAsc = studentList.OrderBy(s => s.StudentName).ThenBy(s => s.Age);
+            //var orderedByNameDesc = studentList.OrderBy(s => s.StudentName).ThenByDescending(s => s.Age);
+
+            //IsYougerThan isYougerThan =  (s, minAge) => s.Age < minAge; // ar mushaobs
+
+            //Print print = () => 
+            //{
+            //    Console.WriteLine("test");
+            //    Console.WriteLine("skjadhajs");
+            //};
+            //var teenagerStudents = studentList.Where(s => s.Age >= 13 && s.Age <= 17);
+
+            IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+            new Student() { StudentID = 2, StudentName = "Moin",  Age = 13 } ,
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 20 } ,
+            new Student() { StudentID = 4, StudentName = "John" , Age = 20} ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
+
+            //var groupedStudents = from student in studentList
+            //                      group student by student.Age;
+            //var groupedStudents = studentList.GroupBy(s => s.Age); // Can not mutate
+            var groupedStudents = studentList.ToLookup(s => s.Age); // Can mutate
+
+            
+
+            foreach (var studentGroup in groupedStudents)
+            {
+                Console.WriteLine($"Age Group {studentGroup.Key}");
+                foreach (var item in studentGroup)
+                {
+                    Console.WriteLine($"Student name {item.StudentName}");
+                }
+            }
+
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var test = numbers.ToLookup(n => n % 2 == 0 ? "Even" : "Odd");
+        }
+
+        delegate void Print();
+        delegate bool IsYougerThan(Student student, int minAge);
+
+    }
+
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+        public int AllowedHeartRate()
+        {
+            return (220 - Age) * 70/100;
+        }
+    }
+}
